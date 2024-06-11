af9239cc1dc92861ec3080efa65077b9a5608ab7
Sineth Sandaruwan <sine41867@gmail.com>
2024-06-11 22:18:02 +0530

    Add function

---
+++ app/models/camera_handler.py
@@ -0,0 +1,14 @@
+import cv2
+
+class CameraHandler:
+    def gen_frames():  
+        camera = cv2.VideoCapture(0)  # Use 0 for web camera
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
